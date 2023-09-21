a927fa3af428948968b1da99d52e31858b6900f5
jinsung-cho <cjs97111@gmail.com>
2023-09-21 13:51:24 +0900

    refactor: clean up unnecessary code

--- apiServer-container/main.go
+++ apiServer-container/main.go
@@ -9,10 +9,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 	env_err := godotenv.Load(".env")
 	util.CheckRuntimeError(env_err, ".env Load fail")
-	
-	// routeHandler := router.InitRouter()
-	http.ListenAndServe(":" + os.Getenv("HOST_PORT"), router.InitRouter())
+
+	http.ListenAndServe(":"+os.Getenv("HOST_PORT"), router.InitRouter())
 }
