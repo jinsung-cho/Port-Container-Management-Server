1f690f1741f236c8538456ea883e362056a9bf19
jinsung-cho <cjs97111@gmail.com>
2023-09-27 14:11:01 +0900

    fix: routing path

--- dbServer-container/router/router.go
+++ dbServer-container/router/router.go
@@ -22,14 +22,17 @@ func InitRouter() http.Handler {
 
 	// Container routes
 	r.HandleFunc("/container", controller.GetContainer).Methods("GET")
-	r.HandleFunc("/container", controller.GetContainer).Methods("POST")
-	r.HandleFunc("/container/spec", controller.GetContainerSpec).Methods("GET")
+	r.HandleFunc("/containers", controller.GetAllContainers).Methods("GET")
+	r.HandleFunc("/container", controller.CreateContainer).Methods("POST")
+	r.HandleFunc("/container/spec", controller.GetAllContainerSpec).Methods("GET")
 	r.HandleFunc("/container/spec/review", controller.GetContainerReview).Methods("GET")
 	r.HandleFunc("/container/spec/review", controller.AppendContainerReview).Methods("POST")
 
 	// Security checkpoint routes
-	r.HandleFunc("/checkpoint", controller.GetCheckpoint).Methods("GET")
-	r.HandleFunc("/checkpoint/state", controller.GetCheckpointState).Methods("GET")
+	r.HandleFunc("/checkpoint", controller.GetAllCheckpoint).Methods("GET")
+	r.HandleFunc("/checkpoint", controller.CreateCheckpoint).Methods("POST")
+	r.HandleFunc("/checkpoint/state", controller.GetAllCheckpointState).Methods("GET")
+	r.HandleFunc("/checkpoint/state", controller.CreateCheckpointState).Methods("POST")
 
 	handler := corsConfig.Handler(r)
 
