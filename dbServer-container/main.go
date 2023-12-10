77c87c181d3821397808976e0973554318fdfe69
jinsung-cho <cjs97111@gmail.com>
2023-12-10 21:12:13 +0900

    fix: delete unnecessary code

--- dbServer-container/main.go
+++ dbServer-container/main.go
@@ -6,14 +6,9 @@ import (
 	"db-server/util"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	env_err := godotenv.Load(".env")
-	util.CheckRuntimeError(env_err, ".env Load fail")
-
 	model.InitDB()
 	err := http.ListenAndServe(":"+os.Getenv("DB_SERVER_PORT"), router.InitRouter())
 	util.CheckRuntimeError(err, "http open err")
